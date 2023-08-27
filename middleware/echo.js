e147be00e372ef078e2fea85bcc7d1715d58b3f5
xiakele <xiakele123@gmail.com>
2023-08-27 22:25:26 +0800

    [功能] echo增加猫娘功能

--- middleware/echo.js
+++ middleware/echo.js
@@ -1,10 +1,14 @@
 module.exports = async function echo (ctx) {
   const command = ctx.message.text
-  const regex = /\/echo\s(.+)/
+  const regex = /\/echo\s(-M\s)?(.+)/
   if (!regex.test(command)) {
     ctx.reply('不知道该echo什么喵')
     return
   }
-  const content = regex.exec(command)[1]
-  ctx.reply(content)
+  const content = regex.exec(command)[2]
+  if (regex.exec(command)[1] || content.includes('猫')) {
+    ctx.reply(content + '喵～')
+  } else {
+    ctx.reply(content)
+  }
 }
