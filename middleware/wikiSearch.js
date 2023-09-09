faf38f780f494cd786be29165e1fa6b2fa599866
xiakele <xiakele123@gmail.com>
2023-09-09 21:58:31 +0800

    [修复] 优化维基百科跳转

--- middleware/wikiSearch.js
+++ middleware/wikiSearch.js
@@ -15,10 +15,9 @@ export default async function (ctx, page) {
   const query = regex.exec(msg.text)[1]
   await page.goto(`https://zh.wikipedia.org/w/index.php?fulltext=1&search=${query}`, { waitUntil: 'domcontentloaded' })
   const result = await page.$eval('.mw-search-result-heading > a', item => {
-    // return { title: item.title, url: item.href, simpUrl: item.href.replace('/wiki/', '/zh-cn/') }
     return { title: item.title, url: item.href }
   })
-  await page.goto(result.url.replace('/wiki/', '/zh-cn/'))
+  await page.goto(result.url.replace('/wiki/', '/zh-cn/'), { waitUntil: 'domcontentloaded' })
   const simpResult = { title: await page.$eval('.mw-page-title-main', item => item.innerHTML), url: page.url() }
   await ctx.replyWithHTML(`搜索结果如下：\n中文：<a href='${result.url}'>${result.title}</a>\n简中：<a href='${simpResult.url}'>${simpResult.title}</a>`, { reply_to_message_id: msg.id })
 }
