c45c1311b5f729aae5185f034fd110d23c522074
xiakele <xiakele123@gmail.com>
2023-09-09 23:13:06 +0800

    [修复] 删除冗余代码

--- middleware/googleSearch.js
+++ middleware/googleSearch.js
@@ -1,25 +1,17 @@
 export default async function (ctx, page) {
   const regex = /\/google\s(.+)/
-  const msg = {
-    text: ctx.message.text,
-    from: {
-      id: ctx.message.from.id,
-      firstName: ctx.message.from.first_name
-    },
-    id: ctx.message.message_id
-  }
-  if (!regex.test(msg.text)) {
-    await ctx.reply('请输入搜索内容', { reply_to_message_id: msg.id })
+  if (!regex.test(ctx.message.text)) {
+    await ctx.reply('请输入搜索内容', { reply_to_message_id: ctx.message.message_id })
     return
   }
-  const query = regex.exec(msg.text)[1]
+  const query = regex.exec(ctx.message.text)[1]
   await page.goto(`https://www.google.com/search?q=${query}`, { waitUntil: 'domcontentloaded' })
   if (page.url().includes('sorry/index')) {
-    await ctx.reply('Google不让人家访问了喵！', { reply_to_message_id: msg.id })
+    await ctx.reply('Google不让人家访问了喵！', { reply_to_message_id: ctx.message.message_id })
     return
   }
   const result = await page.$eval('.LC20lb', item => {
     return { title: item.innerHTML, url: item.parentNode.href }
   })
-  await ctx.replyWithHTML(`搜索结果如下：\n<a href='${result.url}'>${result.title}</a>`, { reply_to_message_id: msg.id })
+  await ctx.replyWithHTML(`搜索结果如下：\n<a href='${result.url}'>${result.title}</a>`, { reply_to_message_id: ctx.message.message_id })
 }
--- middleware/wikiSearch.js
+++ middleware/wikiSearch.js
@@ -1,23 +1,15 @@
 export default async function (ctx, page) {
   const regex = /\/wiki\s(.+)/
-  const msg = {
-    text: ctx.message.text,
-    from: {
-      id: ctx.message.from.id,
-      firstName: ctx.message.from.first_name
-    },
-    id: ctx.message.message_id
-  }
-  if (!regex.test(msg.text)) {
-    await ctx.reply('请输入搜索内容', { reply_to_message_id: msg.id })
+  if (!regex.test(ctx.message.text)) {
+    await ctx.reply('请输入搜索内容', { reply_to_message_id: ctx.message.message_id })
     return
   }
-  const query = regex.exec(msg.text)[1]
+  const query = regex.exec(ctx.message.text)[1]
   await page.goto(`https://zh.wikipedia.org/w/index.php?fulltext=1&search=${query}`, { waitUntil: 'domcontentloaded' })
   const result = await page.$eval('.mw-search-result-heading > a', item => {
     return { title: item.title, url: item.href }
   })
   await page.goto(result.url.replace('/wiki/', '/zh-cn/'), { waitUntil: 'domcontentloaded' })
   const simpResult = { title: (await page.title()).match(/(.*) - 维基百科/)[1], url: page.url() }
-  await ctx.replyWithHTML(`搜索结果如下：\n中文：<a href='${result.url}'>${result.title}</a>\n简中：<a href='${simpResult.url}'>${simpResult.title}</a>`, { reply_to_message_id: msg.id })
+  await ctx.replyWithHTML(`搜索结果如下：\n中文：<a href='${result.url}'>${result.title}</a>\n简中：<a href='${simpResult.url}'>${simpResult.title}</a>`, { reply_to_message_id: ctx.message.message_id })
 }
