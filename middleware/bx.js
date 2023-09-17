76099f967f78708ba63a6ecd10ebeadcc0450e69
xiakele <xiakele123@gmail.com>
2023-09-17 14:17:43 +0800

    [修复] 处理@username的情况（暂时不能mention）

--- middleware/bx.js
+++ middleware/bx.js
@@ -2,6 +2,13 @@ async function reply (ctx, bxUser) {
   if (bxUser.id === ctx.from.id) {
     return ctx.reply('怎么敢拜谢自己的啊😡')
   }
+  if (bxUser.username) {
+    return ctx.replyWithHTML(`<a href="tg://user?id=${ctx.message.from.id}">${ctx.message.from.first_name}</a>` +
+      '拜谢了' +
+      `<a href="tg://user">${bxUser.username}</a>` +
+      '！'
+    )
+  }
   return ctx.replyWithHTML(`<a href="tg://user?id=${ctx.message.from.id}">${ctx.message.from.first_name}</a>` +
       '拜谢了' +
       `<a href="tg://user?id=${bxUser.id}">${bxUser.firstName}</a>` +
@@ -18,8 +25,12 @@ export default async function (ctx) {
   if (!(replyToMessage || mentionEntity)) {
     return await ctx.reply('不知道你想拜谢谁喵')
   }
-  if (mentionEntity) {
+  if (mentionEntity.type === 'text_mention') {
     const mentionUser = { id: mentionEntity.user.id, firstName: mentionEntity.user.first_name }
+    return await reply(ctx, mentionUser)
+  }
+  if (mentionEntity.type === 'mention') {
+    const mentionUser = { username: ctx.message.text.slice(mentionEntity.offset, mentionEntity.offset + mentionEntity.length) }
     return await reply(ctx, mentionUser)
   }
   const replyToUser = { id: replyToMessage.from.id, firstName: replyToMessage.from.first_name }
