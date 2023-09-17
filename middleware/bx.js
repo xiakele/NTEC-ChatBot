1551fac661cf1835cce0eceee5e3d7c6678146eb
xiakele <xiakele123@gmail.com>
2023-09-17 14:26:38 +0800

    [修复] 只有reply时无法拜谢

--- middleware/bx.js
+++ middleware/bx.js
@@ -22,17 +22,19 @@ export default async function (ctx) {
   if (ctx.message.entities) {
     mentionEntity = ctx.message.entities.find(item => (item.type === 'mention' || item.type === 'text_mention'))
   }
-  if (!(replyToMessage || mentionEntity)) {
-    return await ctx.reply('不知道你想拜谢谁喵')
+  if (mentionEntity) {
+    if (mentionEntity.type === 'text_mention') {
+      const mentionUser = { id: mentionEntity.user.id, firstName: mentionEntity.user.first_name }
+      return await reply(ctx, mentionUser)
+    }
+    if (mentionEntity.type === 'mention') {
+      const mentionUser = { username: ctx.message.text.slice(mentionEntity.offset, mentionEntity.offset + mentionEntity.length) }
+      return await reply(ctx, mentionUser)
+    }
   }
-  if (mentionEntity.type === 'text_mention') {
-    const mentionUser = { id: mentionEntity.user.id, firstName: mentionEntity.user.first_name }
-    return await reply(ctx, mentionUser)
+  if (replyToMessage) {
+    const replyToUser = { id: replyToMessage.from.id, firstName: replyToMessage.from.first_name }
+    return await reply(ctx, replyToUser)
   }
-  if (mentionEntity.type === 'mention') {
-    const mentionUser = { username: ctx.message.text.slice(mentionEntity.offset, mentionEntity.offset + mentionEntity.length) }
-    return await reply(ctx, mentionUser)
-  }
-  const replyToUser = { id: replyToMessage.from.id, firstName: replyToMessage.from.first_name }
-  return await reply(ctx, replyToUser)
+  return await ctx.reply('不知道你想拜谢谁喵')
 }
