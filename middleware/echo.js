0718b999fb8d9c7df53c25db84e56bda7e2be2a2
xiakele <xiakele123@gmail.com>
2023-11-06 22:22:54 +0800

    [功能] echo时删除发起者消息

--- middleware/echo.js
+++ middleware/echo.js
@@ -5,6 +5,7 @@ export default async function (ctx) {
     await ctx.reply('不知道该echo什么喵')
     return
   }
+  await ctx.deleteMessage(ctx.message.message_id)
   const content = regex.exec(command)[2]
   if (regex.exec(command)[1] || content.includes('猫')) {
     await ctx.reply(content + '喵～')
